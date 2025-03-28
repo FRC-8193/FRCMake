41ba950236d35494fd2578a1af9fa9e6843e5595
FrederickAmpsUp <frederick.ziola@gmail.com>
2025-03-28 09:34:27 -0400

    Update deploy script

--- cmake/modules/frcmake-deploy.sh
+++ cmake/modules/frcmake-deploy.sh
@@ -39,7 +39,8 @@ for LIB in "${LIBRARIES[@]}"; do
 done
 
 TARGET_USER=lvuser
-TARGET_DIR=/home/lvuser
+TARGET_DIR=/home/lvuser/FRCMakeProgram
+ROBOTCOMMAND_DIR=/home/lvuser
 CONTROL_PATH="/tmp/ssh-mux-$TARGET_USER"
 
 # Start a persistent SSH connection
@@ -61,19 +62,55 @@ deploy_program() {
     start_ssh_multiplexing "$target"
 
     echo "Kill running robot code"
+    echo "Create program directory"
     ssh -o ControlPath="$CONTROL_PATH" "$TARGET_USER@$target" ". /etc/profile.d/natinst-path.sh;
-    							       /usr/local/bin/frcKillRobot.sh -t"
+    							       /usr/local/bin/frcKillRobot.sh -t;
+							       mkdir -p $TARGET_DIR"
 
     echo "Copy new robot program"
     scp -o ControlPath="$CONTROL_PATH" "$PROGRAM" "$TARGET_USER@$target:$TARGET_DIR/FRCUserProgram"
+
+    for lib in $LIBRARIES; do
+	echo "Copy library $(basename $lib)"
+	echo "  Check if file needs update..."
+
+	local_sha256=$(sha256sum $lib | cut -d" " -f1)
+	remote_sha256=$(ssh -o ControlPath="$CONTROL_PATH" "$TARGET_USER@$target" "sha256sum $TARGET_DIR/$(basename $lib)" | cut -d" " -f1)
+
+	file_needs_update=0
+
+	if [ ! -z "$local_sha256" ] && [ ! -z "$remote_sha256" ] && [ "$local_sha256" == "$remote_sha256" ]; then
+	    echo "  SHA256 matches, check MD5"
+
+	    local_md5=$(md5sum $lib | cut -d" " -f1)
+	    remote_md5=$(ssh -o ControlPath="$CONTROL_PATH" "$TARGET_USER@$target" "md5sum $TARGET_DIR/$(basename $lib)" | cut -d" " -f1)
+	
+	    if [ ! -z "$local_md5" ] && [ ! -z "$remote_md5" ] && [ "$local_md5" == "$remote_md5" ]; then
+		echo "  MD5 matches"
+            else
+		file_needs_update=1
+		echo "  MD5 does not match"
+	    fi
+        else
+	    file_needs_update=1
+	    echo "  SHA256 does not match"
+	fi
+
+	if [ ! $file_needs_update == 0 ]; then
+	    echo "$(basename $lib) needs update"
+	    scp -o ControlPath="$CONTROL_PATH" "$lib" "$TARGET_USER@$target:$TARGET_DIR/$(basename $lib)"
+        else
+	    echo "$(basename $lib) does not need update"
+	fi
+    done
 
     echo "Kill network console"
     ssh -o ControlPath="$CONTROL_PATH" "$TARGET_USER@$target" "killall -q netconsole-host || :"
 
     echo "Upload new robotCommand"
-    ssh -o ControlPath="$CONTROL_PATH" "$TARGET_USER@$target" "echo $TARGET_DIR/FRCUserProgram >> $TARGET_DIR/robotCommand &&
+    ssh -o ControlPath="$CONTROL_PATH" "$TARGET_USER@$target" "echo \"cd $TARGET_DIR && $TARGET_DIR/FRCUserProgram\" > $ROBOTCOMMAND_DIR/robotCommand &&
 	    						       chmod a+x $TARGET_DIR/FRCUserProgram &&
-							       chmod a+x $TARGET_DIR/robotCommand"
+							       chmod a+x $ROBOTCOMMAND_DIR/robotCommand"
 
     echo "Restart robot code"
     ssh -o ControlPath="$CONTROL_PATH" "$TARGET_USER@$target" "sync && 
