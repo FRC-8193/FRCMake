e9742a692a17817fce72a1170f4b5faf822868f7
FrederickAmpsUp <frederick.ziola@gmail.com>
2025-03-28 09:17:25 -0400

    Update CMake module for library management

--- cmake/modules/FRCMake.cmake
+++ cmake/modules/FRCMake.cmake
@@ -1,11 +1,33 @@
-include(ExternalProject)
-
 set(CUSTOM_WPILIB_PATH "" CACHE STRING "Location of the wpilibsuite/alwpilib GitHub repository locally")
 set(FRC_SEASON "2025" CACHE STRING "Current season (year) of FRC")
 
-set(WPILIB_SEARCH_PATH ${CMAKE_CURRENT_BINARY_DIR}/wpilib /wpilib /allwpilib ~/wpilib ~/allwpilib ${CUSTOM_WPILIB_PATH})
+set(WPILIB_SEARCH_PATH ${CMAKE_CURRENT_BINARY_DIR}/allwpilib /wpilib /allwpilib ~/wpilib ~/allwpilib ${CUSTOM_WPILIB_PATH})
 
-function(get_and_build_wpilib)
+function(frcmake_get_all_shared_dependency_filenames target collected_deps)
+    	# Retrieve the direct link libraries of the target
+    	get_target_property(direct_deps ${target} LINK_LIBRARIES)
+    	if(direct_deps)
+        	foreach(dep IN LISTS direct_deps)
+            		if(TARGET ${dep})
+                		# Get the type of the target
+                		get_target_property(dep_type ${dep} TYPE)
+                		if(dep_type STREQUAL "SHARED_LIBRARY")
+                			# Get the output filename of the dependency
+					set(dep_location $<TARGET_FILE:${dep}>)
+                    			if(dep_location)
+                        			list(APPEND ${collected_deps} ${dep_location})
+                    			endif()
+                		endif()
+                		# Recursively collect dependencies of the current dependency
+                		frcmake_get_all_shared_dependency_filenames(${dep} ${collected_deps})
+            		endif()
+        	endforeach()
+    	endif()
+    	# Return the collected dependencies to the parent scope
+    	set(${collected_deps} ${${collected_deps}} PARENT_SCOPE)
+endfunction()
+
+function(frcmake_get_wpilib)
 	find_path(WPILIB_ROOT
 		NAMES wpilib-config.cmake.in
 		PATHS ${WPILIB_SEARCH_PATH}
@@ -19,7 +41,7 @@ function(get_and_build_wpilib)
 		
 		execute_process(COMMAND git clone --filter=blob:none --no-checkout https://github.com/wpilibsuite/allwpilib ${WPILIB_ROOT})
 		execute_process(COMMAND git -C ${WPILIB_ROOT} sparse-checkout init --cone)
-		execute_process(COMMAND git -C ${WPILIB_ROOT} sparse-checkout set ${WPILIB_MODULES} "cmake" "cscore")
+		execute_process(COMMAND git -C ${WPILIB_ROOT} sparse-checkout set ${WPILIB_MODULES} "cmake" "cameraserver")
 		execute_process(COMMAND git -C ${WPILIB_ROOT} checkout main)
 	else()
 		message(STATUS "AllWPILib found at ${WPILIB_ROOT}")
@@ -34,7 +56,7 @@ function(get_and_build_wpilib)
 	endforeach()
 endfunction()
 
-function(use_wpilib_toolchain)
+function(frcmake_set_toolchain_roborio)
 	find_path(WPILIB_TOOLCHAIN_DIR
 		NAMES toolchain-config.cmake
 		PATHS ~/wpilib/${FRC_SEASON}/roborio
@@ -49,14 +71,17 @@ function(use_wpilib_toolchain)
 	set(CMAKE_TOOLCHAIN_FILE ${WPILIB_TOOLCHAIN_DIR}/${FRC_SEASON}/roborio/toolchain-config.cmake)
 endfunction()
 
-function(generate_deploy_target TARGETNAME TEAM_NUMBER)
+function(frcmake_generate_deploy_target TARGETNAME TEAM_NUMBER)
 	find_path(MOD_PATH
 		NAMES FRCMake.cmake frcmake-deploy.sh
 		PATHS ${CMAKE_MODULE_PATH}
 	)
+	
+	frcmake_get_all_shared_dependency_filenames(${TARGETNAME} dependencies)
 
 	add_custom_target(deploy_${TARGETNAME}
-		COMMAND ${MOD_PATH}/frcmake-deploy.sh ${TEAM_NUMBER} ${CMAKE_CURRENT_BINARY_DIR}/${TARGETNAME} || true
+		COMMAND ${MOD_PATH}/frcmake-deploy.sh ${TEAM_NUMBER} ${CMAKE_CURRENT_BINARY_DIR}/${TARGETNAME} ${dependencies} || true
+		COMMAND_EXPAND_LISTS
 		COMMENT "Deploying ${TARGETNAME} to Rio"
 	)
 
